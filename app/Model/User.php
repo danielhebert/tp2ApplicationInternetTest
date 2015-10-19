4f6db76c21affd52a22f5fc275b314a6be559baa
Daniel <Daniel@Daniel-PC-IV.zyxel.com>
2015-10-19 17:02:03 -0400

    Test premier commit

---
+++ app/Model/User.php
@@ -0,0 +1,54 @@
+<?php
+App::uses('AppModel', 'Model');
+App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
+/**
+ * User Model
+ *
+ * @property Article $Article
+ */
+class User extends AppModel {
+
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Role' => array(
+			'className' => 'Role',
+			'foreignKey' => 'role_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+ 
+	public $hasMany = array(
+		'Article' => array(
+			'className' => 'Article',
+			'foreignKey' => 'user_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+    public function beforeSave($options = array()) {
+        if (isset($this->data[$this->alias]['password'])) {
+            $passwordHasher = new BlowfishPasswordHasher();
+            $this->data[$this->alias]['password'] = $passwordHasher->hash(
+                    $this->data[$this->alias]['password']
+            );
+        }
+        return true;
+    }
+}
